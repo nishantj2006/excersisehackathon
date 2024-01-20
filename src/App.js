0181492125997ce3e3de57b8b84de055d0653021
Brandon <brandonshen123@gmail.com>
2024-01-19 19:36:24 -0800

    Added basic routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import Register from './Pages/Register';
+import CreateGoal from './Pages/CreateGoal';
+import ReportProgress from './Pages/ReportProgress';
 
 function App() {
   return (
@@ -10,9 +15,19 @@ function App() {
       <Router>
         <nav>
           <Link to="/"> Home </Link>
+          <Link to="/dashboard"> Dashboard </Link>
+          <Link to="/creategoal"> Create Goal </Link>
+          <Link to="/reportprogress"> Report Progress </Link>
+          <Link to="/login"> Login </Link>
+          <Link to="/register"> Register </Link>
         </nav>
         <Routes>
           <Route path="/" element={Home} />
+          <Route path="/login" element={Login} />
+          <Route path="/dashboard" element={Dashboard} />
+          <Route path='/register' element={Register} />
+          <Route path='/creategoal' element={CreateGoal} />
+          <Route path='/reportprogress' element={ReportProgress} />
         </Routes>
       </Router>
     </div>
