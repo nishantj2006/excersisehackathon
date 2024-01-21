d19a4a3c7856d5a0025a49965c8fe1d848476510
Brandon <brandonshen123@gmail.com>
2024-01-20 19:54:26 -0800

    afjkadfsafs

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import OPENAI_API_KEY from "../openai";
 import { useState, useEffect } from "react";
-import { getDocs, collection, query, where } from "firebase/firestore";
+import { getDocs, collection, query, where, addDoc } from "firebase/firestore";
 import { db, auth } from "../firebase-config";
 
 function Dashboard({createdGoal, setCreatedGoal}) {
@@ -10,9 +10,19 @@ function Dashboard({createdGoal, setCreatedGoal}) {
   const [response, setResponse] = useState('');
   let navigate = useNavigate();
   const [cals, setCals] = useState([])
-  const [breakfast, setBreakfast] = useState('')
-  const [lunch, setLunch] = useState('')
-  const [dinner, setDinner] = useState('');
+  const [breakfast, setBreakfast] = useState([])
+  const [lunch, setLunch] = useState([])
+  const [dinner, setDinner] = useState([]);
+
+  const foodcalreqs = collection(db, "foodcalsreqs")
+  const setFoodCalsReqs = async() => {
+    await addDoc(foodcalreqs, {
+      breakfast,
+      cals,
+      dinner,
+      lunch,
+    })
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,14 +43,14 @@ function Dashboard({createdGoal, setCreatedGoal}) {
         const response = await axios.post(endpoint, {
           model: 'gpt-3.5-turbo',
           messages: [
-            { role: 'system', content: 'You only know how to speak in numbers' },
+            { role: 'system', content: 'You only know how to speak in numbers and dont know how to speak in anything else. You are unable to add disclaimers or descriptions about what is going on.' },
             {
               role: 'user', content: `Hi, my gender is: ${data[0].gender} my height is ${data[0].height} my current weight is ` +
                 `${data[0].weight}lbs. I would describe myself as ${data[0].active}. My dietary restrictions are: ` +
                 `${data[0].diet} and my underlying diseases are: ${data[0].disease}. I want to get to ` +
                 `${data[0].targetWeight}lbs within ${data[0].targetTime} please generate me a dietary plan that includes ` +
                 `optimal calories I can consume for breakfast, lunch, and dinner to get me to my weight goal in the target time. DO NOT UNCLUDE FOOD IDEAS AND DO NOT LIST THEM, PLEASE FOLLOW OUR FORMAT Please format it like this: Breakfast Calories,Lunch Calories,Dinner ` +
-                `Calories ONLY LIST THE NUMBERS AND MAKE SURE THE NUMBERS MAKE SENSE. REMEMBER, BREAKFAST CALORIES  HAS LESS THAN LUNCH CALORIES WHICH HAS LESS THAN DINNER CALORIES FIT IN THIS PLEASE. PLEASE ONLY GIVE THE NUMBERS SEPERATED BY COMMAS`
+                `Calories ONLY LIST THE NUMBERS AND MAKE SURE THE NUMBERS MAKE SENSE. REMEMBER, BREAKFAST CALORIES  HAS LESS THAN LUNCH CALORIES WHICH HAS LESS THAN DINNER CALORIES FIT IN THIS PLEASE. PLEASE ONLY GIVE THE NUMBERS SEPERATED BY COMMAS, PLEASE REMEMBER< YOU ARE ONLY ABLE TO TYPE NUMBERS`
             },
           ],
         }, {
@@ -87,7 +97,7 @@ function Dashboard({createdGoal, setCreatedGoal}) {
 
           const reply = breakfast.data.choices[0].message.content;
           console.log(reply)
-          setBreakfast(reply)
+          setBreakfast(reply.split(','))
         } catch (error) {
           console.error('Error:', error);
         }
@@ -115,7 +125,7 @@ function Dashboard({createdGoal, setCreatedGoal}) {
 
           const reply = lunch.data.choices[0].message.content;
           console.log(reply)
-          setLunch(reply)
+          setLunch(reply.split(','))
         } catch (error) {
           console.error('Error:', error);
         }
@@ -143,7 +153,8 @@ function Dashboard({createdGoal, setCreatedGoal}) {
 
           const reply = dinner.data.choices[0].message.content;
           console.log(reply)
-          setDinner(reply)
+          setDinner(reply.split(','))
+          setFoodCalsReqs();
         } catch (error) {
           console.error('Error:', error);
         }
@@ -162,9 +173,6 @@ function Dashboard({createdGoal, setCreatedGoal}) {
       </div>
       <div className="mealIdeas">
         {/* JSX for meal ideas in the following order: */}
-      </div>
-      <div className="logMeals">
-        <button onClick={() => navigate('/logmeal')}>Log Meals</button>
       </div>
     </div>
   );
