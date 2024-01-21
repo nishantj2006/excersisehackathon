b51b3f8c28b76c39e520e31989369a30b56e41e3
Benson Tran <135925601+BensonTran12@users.noreply.github.com>
2024-01-20 19:54:50 -0800

    small changes

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -33,12 +33,12 @@ function Dashboard({createdGoal, setCreatedGoal}) {
         const response = await axios.post(endpoint, {
           model: 'gpt-3.5-turbo',
           messages: [
-            { role: 'system', content: 'You only know how to speak in numbers' },
+            { role: 'system', content: 'YOU ONLY KNOW HOW TO SPEAK IN NUMBERS' },
             {
-              role: 'user', content: `Hi, my gender is: ${data[0].gender} my height is ${data[0].height} my current weight is ` +
-                `${data[0].weight}lbs. I would describe myself as ${data[0].active}. My dietary restrictions are: ` +
-                `${data[0].diet} and my underlying diseases are: ${data[0].disease}. I want to get to ` +
-                `${data[0].targetWeight}lbs within ${data[0].targetTime} please generate me a dietary plan that includes ` +
+              role: 'user', content: `Hi, my gender is: ${data.gender} my height is ${data.height} my current weight is ` +
+                `${data.weight}lbs. I would describe myself as ${data.active}. My dietary restrictions are: ` +
+                `${data.diet} and my underlying diseases are: ${data.disease}. I want to get to ` +
+                `${data.targetWeight}lbs within ${data.targetTime} please generate me a dietary plan that includes ` +
                 `optimal calories I can consume for breakfast, lunch, and dinner to get me to my weight goal in the target time. DO NOT UNCLUDE FOOD IDEAS AND DO NOT LIST THEM, PLEASE FOLLOW OUR FORMAT Please format it like this: Breakfast Calories,Lunch Calories,Dinner ` +
                 `Calories ONLY LIST THE NUMBERS AND MAKE SURE THE NUMBERS MAKE SENSE. REMEMBER, BREAKFAST CALORIES  HAS LESS THAN LUNCH CALORIES WHICH HAS LESS THAN DINNER CALORIES FIT IN THIS PLEASE. PLEASE ONLY GIVE THE NUMBERS SEPERATED BY COMMAS`
             },
@@ -162,10 +162,7 @@ function Dashboard({createdGoal, setCreatedGoal}) {
       </div>
       <div className="mealIdeas">
         {/* JSX for meal ideas in the following order: */}
-      </div>
-      <div className="logMeals">
-        <button onClick={() => navigate('/logmeal')}>Log Meals</button>
-      </div>
+      </div>  
     </div>
   );
 }
